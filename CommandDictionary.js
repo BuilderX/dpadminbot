8295efa43642c329cf030fd5198d947b9cd87636
Raymond Gonzalez <raymondgon7@gmail.com>
2017-07-24 12:36:23 -0400

    Update CommandDictionary.js

--- CommandDictionary.js
+++ CommandDictionary.js
@@ -3,10 +3,10 @@ var BOTCOMMANDS = {}
 // didn't have much time at work will add more later.
 
 function addNewCommand(CommandName, commandVal){
-        BOTCOMMANDS[CommandName] = commandVal;
+   BOTCOMMANDS[CommandName] = commandVal;
 }
 function deleteCommand(CommandName, commandVal){
-        delete BOTCOMMANDS[CommandName];
+   delete BOTCOMMANDS[CommandName];
 }
 
 //test 
@@ -25,27 +25,26 @@ function roleFormation(role){
 
 //add a memeber to a role
 function roleSearc(name,role){
- let role = message.guild.roles.find(name,role);
+   let role = message.guild.roles.find(name,role);
 }
 function memberGrab(){
-        let member = message.mentions.members.first();
-           return memeber;
+   let member = message.mentions.members.first();
+   return memeber;
 }
 function addMemberRole(role, memeber){
-      member.addRole(role).catch(console.error);
+   member.addRole(role).catch(console.error);
 }
-
 function rmMemberRole(role, memeber){
-      member.removeRole(role).catch(console.error);
+   member.removeRole(role).catch(console.error);
 }
 
 function getAllMemberPermissions(){
-        let perm = message.memeber.permissions;
+   let perm = message.memeber.permissions;
 }
 function checkPermissions(perm){
-        let has_certain_permission = message.member.hasPermission(perm);
-        //log message 
-        console.log(has_certain_permissions);
+   let has_certain_permission = message.member.hasPermission(perm);
+    //log message 
+   console.log(has_certain_permissions);
 }
 
 addNewCommand("find role", roleSearch);
