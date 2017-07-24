81dd7a280978ed5c5c2d7b490b0addeedb7b4d14
Raymond Gonzalez <raymondgon7@gmail.com>
2017-07-24 12:16:48 -0400

    Update COMMANDDICTIONARY.js

--- COMMANDDICTIONARY.js
+++ COMMANDDICTIONARY.js
@@ -1,1 +1,12 @@
+var BOTCOMMANDS = {}
+
+
+function addNewCommand(CommandName, commandVal){
+        BOTCOMMANDS[CommandName] = commandVal;
+}
+function deleteCommand(CommandName, commandVal){
+        delete BOTCOMMANDS[CommandName];
+}
+
+
 
