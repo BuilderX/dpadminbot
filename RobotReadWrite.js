18094d36d983bd412fd69208050649d5313d24de
Raymond Gonzalez <16975502+BuilderX@users.noreply.github.com>
2019-12-03 09:47:24 -0500

    Update RobotReadWrite.js

--- RobotReadWrite.js
+++ RobotReadWrite.js
@@ -3,12 +3,13 @@ const fs = require('fs');
 let userPoints = points[message.author.id] ? points[message.author.id].points : 0;
  
 function readJsonFile(path, info){
-  let points = JSON.parase(fs.readFileSync(path,"utf8"));
+  let points = JSON.parse(fs.readFileSync(path,"utf8"));
+      console.log(points);
   // print in message console
 }
 
 function writeToJsonFile(path, client){
-     let points = JSON.parase(fs.readFileSync(path,"utf8"));
+     let points = JSON.parse(fs.readFileSync(path,"utf8"));
      client.on("message",function(){
           if(message.author.bot) return;
           if(message.length > 0 ){
